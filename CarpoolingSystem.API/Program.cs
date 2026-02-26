6f0f13af6a915447e1595f1fd169cf4cd379a2e3
Abhishree Gahlot <abhishree.g@intimetec.com>
2026-02-26 11:24:55 +0530

    feat: configure CORS to allow Angular frontend connection

--- CarpoolingSystem.API/Program.cs
+++ CarpoolingSystem.API/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
@@ -21,5 +22,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.MapControllers();
 app.Run();
