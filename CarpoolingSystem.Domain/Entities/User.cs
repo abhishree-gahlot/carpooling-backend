e947015149eb60d2a98bdfd0766939b7cadee589
Abhishree Gahlot <abhishree.g@intimetec.com>
2026-02-26 16:52:25 +0530

    feat(domain): add entity models for Vehicle, User

---
+++ CarpoolingSystem.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarpoolingSystem.Domain.Enums;
+
+namespace CarpoolingSystem.Domain.Entities
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public required string EmailId { get; set; } 
+        public required string UserName { get; set; }
+        public UserRole UserRole { get; set; }
+        public string Pin { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsActive { get; set; } = true;
+    }
+}
