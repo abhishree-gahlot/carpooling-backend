ea1e0c1b2b79c5ba756e97aabf06ac9fc8a7045b
Abhishree Gahlot <abhishree.g@intimetec.com>
2026-02-26 10:43:08 +0530

    Configure CORS policy for Angular frontend

--- CarpoolingSystem.API/Program.cs
+++ CarpoolingSystem.API/Program.cs
@@ -2,9 +2,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AngularPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 
+app.UseCors("AngularPolicy");
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
