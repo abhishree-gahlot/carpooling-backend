db35d691727bc8b7ff2651822ff41a8b70ade39e
HemanthN-ITT <hemanth.n@intimetec.com>
2026-03-12 16:35:26 +0530

    Resolved the Automapper Conflict

--- CarpoolingSystem.API/Program.cs
+++ CarpoolingSystem.API/Program.cs
@@ -9,10 +9,13 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using AutoMapper;
+//using CarpoolingSystem.Application.Mapping;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+//builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+//builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ITokenService, JwtTokenService>();
@@ -23,10 +26,14 @@ builder.Services.AddScoped<IRideSessionService, RideSessionService>();
 builder.Services.AddScoped<CarpoolingSystem.Domain.Repositories.IVehicleRepository, VehicleRepository>();
 builder.Services.AddScoped<IVehicleRepository, VehicleRepository>();
 builder.Services.AddScoped<IVehicleService, VehicleService>();
+
+//builder.Services.AddAutoMapper(cfg => { }, typeof(VehicleProfile).Assembly);
+//builder.Services.AddAutoMapper(typeof(VehicleProfile));
+builder.Services.AddAutoMapper(cfg => { }, typeof(VehicleProfile).Assembly);
 //builder.Services.AddScoped<CarpoolingSystem.Application.Interfaces.IVehicleRepository, VehicleRepository>();
 
-builder.Services.AddAutoMapper(
-    typeof(CarpoolingSystem.Application.Mappings.VehicleProfile).Assembly);
+//builder.Services.AddAutoMapper(
+//    typeof(CarpoolingSystem.Application.Mappings.VehicleProfile).Assembly);
 
 var jwtSettings = builder.Configuration.GetSection("Jwt");
 var key = Encoding.ASCII.GetBytes(jwtSettings["Key"]!);
